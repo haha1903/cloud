362ca0e87b0bd1d73f2808d499e47e8acbe9c166
hai.chang <hai.chang@datayes.com>
2013-08-14 17:33:06 +0800

    delete tenant

--- src/main/java/com/datayes/cloud/StorageManager.java
+++ src/main/java/com/datayes/cloud/StorageManager.java
@@ -1,8 +1,11 @@
 package com.datayes.cloud;
 
 import com.datayes.cloud.access.Volume;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.SimpleType;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * User: changhai
@@ -14,25 +17,33 @@ public class StorageManager {
     public static final int MAX_WAIT = 10;
     public static final int INTERVAL = 1000;
     public static final String AVAILABLE = "available";
-    private OpenstackContext openstackContext;
+    private OpenstackContext ctx;
 
     public StorageManager(OpenstackContext openstackContext) {
-        this.openstackContext = openstackContext;
+        this.ctx = openstackContext;
     }
 
     public Volume createVolume(Volume volume) throws IOException, InterruptedException {
-        Volume result = openstackContext.post("http://10.20.112.226:8776/v1/" + openstackContext.getTenant().getId() + "/volumes", "volume", volume, "volume", Volume.class);
+        Volume result = ctx.post(ctx.getVolumeUrl() + "/volumes", "volume", volume, "volume", Volume.class);
         Volume status = null;
         for (int i = 0; i < MAX_WAIT; i++) {
-            status = openstackContext.get("http://10.20.112.226:8776/v1/" + openstackContext.getTenant().getId() + "/volumes/" + result.getId(), "volume", Volume.class);
+            status = ctx.get(ctx.getVolumeUrl() + "/volumes/" + result.getId(), "volume", Volume.class);
             if (isAvailable(status)) break;
             Thread.sleep(INTERVAL);
         }
-        if(!isAvailable(status)) throw new IOException("create volume failure, volume = " + volume);
+        if (!isAvailable(status)) throw new IOException("create volume failure, volume = " + volume);
         return result;
     }
 
     private boolean isAvailable(Volume status) {
         return AVAILABLE.equals(status.getStatus());
+    }
+
+    public List<Volume> listVolumes() throws IOException {
+        return ctx.get(ctx.getVolumeUrl() + "/volumes", "volumes", CollectionType.construct(List.class, SimpleType.construct(Volume.class)));
+    }
+
+    public void deleteVolume(String volumeId) throws IOException {
+        ctx.delete(ctx.getVolumeUrl() + "/volumes/" + volumeId);
     }
 }
--- src/main/java/com/datayes/cloud/TenantManager.java
+++ src/main/java/com/datayes/cloud/TenantManager.java
@@ -1,8 +1,6 @@
 package com.datayes.cloud;
 
-import com.datayes.cloud.access.Role;
-import com.datayes.cloud.access.Tenant;
-import com.datayes.cloud.access.User;
+import com.datayes.cloud.access.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -13,26 +11,39 @@ import java.util.List;
  * Time: 下午4:13
  */
 public class TenantManager {
-    private final OpenstackContext openstackContext;
+    private final OpenstackContext ctx;
 
     public TenantManager(OpenstackContext openstackContext) {
-        this.openstackContext = openstackContext;
+        this.ctx = openstackContext;
     }
 
     public Tenant createTenant(String name, String description, boolean enabled) throws IOException {
         Tenant tenant = new Tenant(name, description, enabled);
-        Tenant result = openstackContext.post("http://10.20.112.226:35357/v2.0/tenants", "tenant", tenant, "tenant", Tenant.class);
-        User user = openstackContext.getUser();
-        Role role = openstackContext.getRole("admin");
-        openstackContext.put("http://10.20.112.226:35357/v2.0/tenants/" + result.getId() + "/users/" + user.getId() + "/roles/OS-KSADM/" + role.getId());
+        Tenant result = ctx.post(ctx.getIdentityAdminUrl() + "/tenants", "tenant", tenant, "tenant", Tenant.class);
+        User user = ctx.getUser();
+        Role role = ctx.getRole("admin");
+        ctx.put(ctx.getIdentityAdminUrl() + "/tenants/" + result.getId() + "/users/" + user.getId() + "/roles/OS-KSADM/" + role.getId());
         return result;
     }
 
     public List<Tenant> listTenants() throws IOException {
-        return openstackContext.listTenants();
+        return ctx.listTenants();
     }
 
     public void deleteTenant(String name) throws IOException {
-        openstackContext.deleteTenant(name);
+        OpenstackContext tenantContext = new OpenstackContext(ctx.getIdentityServiceUrl(), ctx.getUsername(), ctx.getPassword(), name);
+        Tenant tenant = tenantContext.getTenant();
+        if (tenant != null) {
+            ComputeManager computeManager = new ComputeManager(tenantContext);
+            List<Server> servers = computeManager.listServers();
+            for (Server server : servers) computeManager.deleteServer(server.getId());
+            StorageManager storageManager = new StorageManager(tenantContext);
+            List<Volume> volumes = storageManager.listVolumes();
+            for (Volume volume : volumes) storageManager.deleteVolume(volume.getId());
+            NetworkManager networkManager = new NetworkManager(tenantContext);
+            List<Network> networks = networkManager.listNetworks();
+            for (Network network : networks) networkManager.deleteNetwork(network.getId());
+            ctx.delete(ctx.getIdentityAdminUrl() + "/tenants/" + tenant.getId());
+        }
     }
 }
